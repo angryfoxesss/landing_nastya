b48d34c727ff190f513bb399e3ae68b188f03ec9
Юрий Раевский <root1@MacBook-Air.lan>
2021-11-12 20:45:27 +0300

    v0.53

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd06288e03be5371"; }
+/******/ 		__webpack_require__.h = function() { return "0c1c48c406f1efa3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
