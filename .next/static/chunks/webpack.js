455f9346fbcbf00d6e7f57a6e7402f52e456af05
Юрий Раевский <root1@Air-Urij.lan>
2021-11-14 02:58:35 +0300

    alpha v0.80

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "263fb78439b4169d"; }
+/******/ 		__webpack_require__.h = function() { return "d4bf704e398d5c45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
